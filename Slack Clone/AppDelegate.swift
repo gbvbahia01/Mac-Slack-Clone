d062b74d7db013acfc226d560d4640e4860d8ddd
Guilherme Bahia <gbvbahia01@gmail.com>
2017-06-08 21:43:53 -0300

    first commit

---
+++ Slack Clone/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Slack Clone
+//
+//  Created by Guilherme B V Bahia on 08/06/17.
+//  Copyright © 2017 Planet Bang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+   func applicationDidFinishLaunching(_ aNotification: Notification) {
+      // Insert code here to initialize your application
+   }
+
+   func applicationWillTerminate(_ aNotification: Notification) {
+      // Insert code here to tear down your application
+   }
+
+
+}
+
---
+++ Slack Clone/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  Slack Clone
+//
+//  Created by Guilherme B V Bahia on 08/06/17.
+//  Copyright © 2017 Planet Bang. All rights reserved.
+//
+
+import Cocoa
+
+class LoginViewController: NSViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view.
+   }
+
+   override var representedObject: Any? {
+      didSet {
+      // Update the view, if already loaded.
+      }
+   }
+
+
+}
+
